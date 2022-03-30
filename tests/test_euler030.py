b38b15a52247f7784824a306df99f6479f4cc7b0
Dmitriy Poushnoy <d.poushnoy@gmail.comoushnoy@gmail.com>
2022-03-30 19:53:55 +0300

    Completed task euler030

--- tests/test_euler030.py
+++ tests/test_euler030.py
@@ -1,10 +1,17 @@
 import pytest as pytest
 
-from EulerProblems.euler030.euler030 import func
+from EulerProblems.euler030.euler030 import is_power_number, sum_power_numbers
 
 
-def test_func():
-    assert func() == False
+def test_is_power_number():
+    assert is_power_number(1634, 4) == True
+    assert is_power_number(8208, 4) == True
+    assert is_power_number(9474, 4) == True
+    assert is_power_number(1, 4) == False
+
+
+def test_sum_power_numbers():
+    assert sum_power_numbers(4) == 19316
 
 
 if __name__ == '__main__':
