6b2112a65e3d314d3738c95d850e5f7e9a1d9062
Dmitriy Poushnoy <d.poushnoy@gmail.comoushnoy@gmail.com>
2022-04-06 23:17:27 +0300

    Task euler031 completed via dynamic programming in euler031dynamic.py

--- tests/test_euler031.py
+++ tests/test_euler031.py
@@ -1,7 +1,7 @@
 import pytest
 
 from EulerProblems.euler031.euler031 import all_variants_slow
-from EulerProblems.euler031.euler031dynamic import get_list_nominals, make_zero_matrix
+from EulerProblems.euler031.euler031dynamic import get_list_nominals, make_zero_matrix, find_all_variants
 
 
 def test_all_variants():
@@ -15,8 +15,6 @@ def test_all_variants():
     assert all_variants_slow(8) == 7
     assert all_variants_slow(9) == 8
     assert all_variants_slow(10) == 11
-    # assert all_variants_slow(20) == 41
-    # assert all_variants_slow(30) == 109
 
 
 def test_get_list_nominals():
@@ -25,6 +23,22 @@ def test_get_list_nominals():
 
 def test_make_zero_matrix():
     assert make_zero_matrix(5, 4) == [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+
+def test_find_all_variants():
+    assert find_all_variants(200, 1) == 1
+    assert find_all_variants(200, 1) == 1
+    assert find_all_variants(200, 2) == 2
+    assert find_all_variants(200, 3) == 2
+    assert find_all_variants(200, 4) == 3
+    assert find_all_variants(200, 5) == 4
+    assert find_all_variants(200, 6) == 5
+    assert find_all_variants(200, 7) == 6
+    assert find_all_variants(200, 8) == 7
+    assert find_all_variants(200, 9) == 8
+    assert find_all_variants(200, 10) == 11
+    assert find_all_variants(200, 20) == 41
+    assert find_all_variants(200, 30) == 109
 
 
 if __name__ == '__main__':
