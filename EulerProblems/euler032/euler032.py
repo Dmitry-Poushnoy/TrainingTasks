951eded77f5c4f6fa5dbd3c47dac9cdde813b1a7
Dmitriy Poushnoy <d.poushnoy@gmail.comoushnoy@gmail.com>
2022-04-07 22:57:31 +0300

    Add attributes "len" and "pandigital"

--- EulerProblems/euler032/euler032.py
+++ EulerProblems/euler032/euler032.py
@@ -1,7 +1,9 @@
 class DigitNum:
 
-    def __init__(self, number=0):
+    def __init__(self, number: int = 0):
         self.__n = number
+        self.len = self.get_len(self.__n)
+        self.pandigital = self.is_pandigital(self.__n, self.len)
 
     @property
     def n(self):
@@ -11,8 +13,27 @@ class DigitNum:
     def n(self, num):
         self.__n = num
 
+    @staticmethod
+    def get_len(x: int) -> int:
+        """
+        Calculate number of digits in integer number
+        :return: Integer number of digits
+        """
+        return len([i for i in str(x)])
+
+    @staticmethod
+    def is_pandigital(x: int, len_for_pandigital: int = 9) -> bool:
+        '''
+        Check if integer number is pandigital
+        :param x: Integer number to check for pandigitality
+        :param len_for_pandigital: Number for checking (int from 1 to 9)
+        :return: True if the number is pandigital
+        '''
+        list_of_digits = [i for i in str(x)]
+        if '0' in list_of_digits or len(list_of_digits) != len_for_pandigital:
+            return False
+        return len(set(list_of_digits)) == len_for_pandigital
+
 
 if __name__ == '__main__':
-    x = DigitNum(12)
-    x.n = 10
-    print(x.n)
+    pass
--- tests/test_euler032.py
+++ tests/test_euler032.py
@@ -2,16 +2,29 @@ import pytest
 
 from EulerProblems.euler032.euler032 import DigitNum
 
-a = DigitNum()
-
 
 def test_geter_n():
+    a = DigitNum()
     assert a.n == 0
 
 
 def test_setter_n():
+    a = DigitNum()
     a.n = 123
     assert a.n == 123
+
+
+def test_is_pandigital():
+    assert DigitNum.is_pandigital(987654321, 9) == True
+    assert DigitNum.is_pandigital(1223456789, 9) == False
+    assert DigitNum.is_pandigital(1234567, 9) == False
+    assert DigitNum.is_pandigital(987653210) == False
+
+def test_lenght():
+    x2 = DigitNum(65733)
+    assert x2.len == 5
+    x2 = DigitNum(6573223)
+    assert x2.len == 7
 
 
 if __name__ == '__main__':
