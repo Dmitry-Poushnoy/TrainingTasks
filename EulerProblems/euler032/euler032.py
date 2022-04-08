b1ea9d638419193b17b644cc3eb2986f27230059
Dmitriy Poushnoy <d.poushnoy@gmail.comoushnoy@gmail.com>
2022-04-08 20:22:20 +0300

    Complete euler032.py by one function without extra classes

--- EulerProblems/euler032/euler032.py
+++ EulerProblems/euler032/euler032.py
@@ -1,47 +1,21 @@
-class DigitNum(int):
-
-    def __init__(self, number: int = 0):
-        super().__init__()
-        self.__n = number
-        self.length = self.get_len(self.__n)
-        self.pandigital = self.is_pandigital(self.__n)
-
-    @property
-    def n(self):
-        return self.__n
-
-    @n.setter
-    def n(self, num):
-        self.__n = num
-
-    def length(self):
-        return self.length
-
-    @staticmethod
-    def get_len(x: int) -> int:
-        """
-        Calculate number of digits in integer number
-        :return: Integer number of digits
-        """
-        return len(str(x))
-
-    @staticmethod
-    def is_pandigital(x: int) -> bool:
-        """
-        Check if integer number is pandigital
-        :param x: Integer number to check for pandigitality
-        :return: True if the number is pandigital
-        """
-        len_for_pandigital = DigitNum.get_len(x)
-        list_of_digits = [int(i) for i in str(x)]
-        set_of_pandigits = set([i for i in range(1, len_for_pandigital+1)])
-        if 0 in list_of_digits or len(list_of_digits) != len(set_of_pandigits):
-            return False
-        else:
-            return set(list_of_digits) == set_of_pandigits
+def get_sum_pandigital_products():
+    """
+    Find sum of all unique products which are consist of {1-9} like 39 × 186 = 7254
+    :return:
+    """
+    res_list = []
+    d4_set = set()
+    for d2 in range(0, 100):
+        for d3 in range(0, 10000):
+            d4 = d2 * d3
+            d_string = str(d2) + str(d3) + str(d4)
+            candidate_str_set = set(d_string)
+            if len(d_string) == 9 and candidate_str_set == {'1', '2', '3', '4', '5', '6', '7', '8', '9'}:
+                if d4 not in d4_set:
+                    res_list.append([d2, d3, d4])
+                    d4_set.add(d4)
+    return sum([item[2] for item in res_list])
 
 
 if __name__ == '__main__':
-    gen_pandigitals_9 = (DigitNum(i) for i in range(12345678, 87654322) if DigitNum(i).pandigital is True)
-
-
+    print(f"Сумма всех пандигитальных произведений: {get_sum_pandigital_products()}")
--- tests/test_euler032.py
+++ tests/test_euler032.py
@@ -1,31 +1,10 @@
 import pytest
 
-from EulerProblems.euler032.euler032 import DigitNum
+from EulerProblems.euler032.euler032 import get_sum_pandigital_products
 
 
-def test_geter_n():
-    a = DigitNum()
-    assert a.n == 0
-
-
-def test_setter_n():
-    a = DigitNum()
-    a.n = 123
-    assert a.n == 123
-
-
-def test_is_pandigital():
-    assert DigitNum.is_pandigital(987654321) == True
-    assert DigitNum.is_pandigital(13) == False
-    assert DigitNum.is_pandigital(132) == True
-    assert DigitNum.is_pandigital(1223456789) == False
-    assert DigitNum.is_pandigital(1734562) == True
-    assert DigitNum.is_pandigital(987653210) == False
-    assert DigitNum.is_pandigital(123456790) == False
-
-def test_length():
-    x2 = DigitNum(65733)
-    assert x2.length == 5
+def test_get_sum_pandigital_products():
+    assert get_sum_pandigital_products() == 45228
 
 
 if __name__ == '__main__':
