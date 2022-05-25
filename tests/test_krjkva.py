bbc342c5bf9dc9cc2cc2d215f0a3ed4489fada0f
Dmitriy Poushnoy <d.poushnoy@gmail.comoushnoy@gmail.com>
2022-05-25 13:08:19 +0300

    Tak KRJKVA completed

---
+++ tests/test_krjkva.py
@@ -0,0 +1,23 @@
+import pytest
+
+from CodeWars.krjkva import is_krjkva, krj, kva, kvakrj
+
+
+def test_is_krjkva():
+    assert is_krjkva(123156) == True
+    assert is_krjkva(123456) == False
+
+def test_krj():
+    assert krj(123456) == 123
+    assert krj(923456) == 923
+
+def test_kva():
+    assert kva(123456) == 456
+    assert kva(923451) == 451
+
+def test_kvakrj():
+    assert kvakrj(123145) == 145123
+
+
+if __name__ == '__main__':
+    pytest.main()
